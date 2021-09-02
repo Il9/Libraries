dfeb748a8b259a26c5c5dc508d5f0a6d6572a888
Il9 <rntpwn90@gmail.com>
2021-09-02 15:15:25 +0900

    Use eslint-config packages instead of plugins

--- eslint-config-base/index.js
+++ eslint-config-base/index.js
@@ -3,18 +3,13 @@ module.exports = {
     node: true,
     es6: true
   },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module"
-  },
-  plugins: ["@typescript-eslint", "prettier"],
   extends: [
     "eslint:recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended"
   ],
   settings: {
     "import/resolver": {
